08eb9101524f840f1b2e533568b8a250ecb1ae83
josepvindas <13647898+josepvindas@users.noreply.github.com>
2019-05-12 12:52:04 -0600

    inicio de la implementacion de la logica

---
+++ BiblioTEC/Logic/requestManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioTEC.Logic
+{
+    public class requestManager
+    {
+        private restClient client;
+        private string IP = "";
+        private string PORT = "";
+        private string URL = "";
+
+        /*
+         *  Constructor para la clase request manager
+         */
+        public requestManager()
+        {
+            // Se crea la instancia del cliente
+            this.client = new restClient();
+            // Se especifican la IP y puerto a los que se desea conectar
+            this.IP = "";
+            this.PORT = "";
+            // Se arma el URL
+            this.URL = "http://" + IP + ":" + PORT + "";
+
+        }
+
+        /*
+         *  LogIn del usuario
+         */
+        public string[] logIn(string id, string password)
+        {
+            // string de respuesta
+            string idUsuario = "";
+            string tipoUsuario = "";
+            string[] result = new string[2];
+
+            // Consulta
+            string requestType = "loginEstudiante/";
+            this.URL = this.URL + requestType + id + "/" + password;
+            this.client.endPoint = this.URL;
+            string getrequest = client.makeRequest(1);
+            getrequest = getrequest.Replace("\"", "'");
+            dynamic jsonResult = JsonConvert.DeserializeObject(getrequest);
+            idUsuario = jsonResult[0].id;
+            tipoUsuario = jsonResult[0].tipo;
+
+            // Llenado del array
+            result[0] = idUsuario;
+            result[1] = tipoUsuario;
+
+            // Resultado de la consulta
+            return result;
+        }
+
+
+    }
+}
